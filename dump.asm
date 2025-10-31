99e6a49eb3c7a11ae18d9f9f8ab16ca7883b1774
SUEYASU Taizo <tsueyasu@gmail.com>
2025-11-01 00:57:25 +0900

    論理／物理メモリーダンププログラム

---
+++ dump.asm
@@ -0,0 +1,20 @@
+        MOVI    R8, lmem_msg
+        SYSCALL 1
+
+        MOVI    R8, 0x00000     ; 論理アドレス
+        MOVI    R9, 1
+        SYSCALL 40
+
+        MOVI    R8, pmem_msg
+        SYSCALL 1
+
+        MOVI    R8, 0x00000     ; 物理アドレス
+        MOVI    R9, 0
+        SYSCALL 40
+
+        RET
+
+lmem_msg:
+        .STRING "[Logical Memory Dump]\n"
+pmem_msg:
+        .STRING "[Physical Memory Dump]\n"
